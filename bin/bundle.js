2e9ddad3b8eefb9b69523496bc7e5590220bd133
Sergei Dyshel <sergei@dyshel.com>
2024-11-28 12:09:40 +0200

    tweaks

--- bin/bundle.js
+++ bin/bundle.js
@@ -1,16 +1,11 @@
 const {build} = await import('esbuild')
 await build({
   bundle: true,
-  entryPoints: ['./src/**/*.ts'],
-  // Mark shelljs as an external dependency. Plugin-plugins v5 removes the shelljs dependency so we can remove
-  // this once that's been released.
-  external: ['shelljs'],
+  entryPoints: ['./src/index.ts'],
   format: 'esm',
   inject: ['./bin/cjs-shims.js'],
   loader: {'.node': 'copy'},
   outdir: './dist',
   platform: 'node',
   plugins: [],
-  splitting: true,
-  treeShaking: true,
 })
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
 import {Command, execute} from '@oclif/core'
-import * as PluginPlugins from '@oclif/plugin-plugins'
-import * as PluginTestEsm1 from '@oclif/plugin-test-esm-1'
 
 import ESBuild from './commands/esbuild.js'
 import Hello from './commands/hello/index.js'
 import HelloWorld from './commands/hello/world.js'
 export {default as INIT_HOOK} from './hooks/init/init.js'
 
-export const ESM1_INIT_HOOK = PluginTestEsm1.hooks.init
-export const UPDATE_HOOK = PluginPlugins.hooks.update
-
 export const COMMANDS: Record<string, Command.Class> = {
-  ...PluginTestEsm1.commands,
-  ...PluginPlugins.commands,
   esbuild: ESBuild,
   hello: Hello,
   'hello:alias': HelloWorld,
